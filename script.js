a5300c90b1b8c174b1989682fe889937c844b5cf
Paula-Carlech <89168944+Paula-Carlech@users.noreply.github.com>
2022-11-17 21:09:48 -0300

    New updates

---
+++ script.js
@@ -0,0 +1,8 @@
+var qtd = 0;
+
+qtd = document.getElementsByClassName("quantidade").innerText = qtd;
+
+function menos(){
+    qtd = qtd - 1;
+    document.getElementsByClassName("quantidade").innerText = qtd;
+}
